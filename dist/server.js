baa1071a5c3f9e726a6798740349374f9923fa4d
Etan Joseph Heyman <etan@heyman.net>
2022-10-06 10:14:46 -0500

    now?

--- dist/server.js
+++ dist/server.js
@@ -3,15 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const express = require('express');
 require('dotenv').config();
 const uri = process.env.MONGODB_URI;
-const app = express_1.default();
+const app = express();
 const port = process.env.PORT || 3000;
-app.use(express_1.default.static('public'));
-app.use(express_1.default.json());
+app.use(express.static('public'));
+app.use(express.json());
 app.use(cookie_parser_1.default());
 mongoose_1.default
     .connect(uri)
--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
-import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+const express = require('express');
 
 require('dotenv').config();
 
